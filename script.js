672432f11be1b406dfdb49cc7d8b1a0c6f6faa70
Tanmoy Mazumdar <tmazumdar0@gmail.com>
2021-10-05 19:10:34 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("getamount").innerText=principal;
+    document.getElementById("interest").innerText=rate;
+    document.getElementById("res").innerText=interest;
+    document.getElementById("calculate").innerText=year;
     
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+document.addEventListener('change',updateRate);
+document.addEventListener('onclick',compute);
